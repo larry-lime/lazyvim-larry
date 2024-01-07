b21fd5da117d9fe09f2807cfa28c31860e65e063
Lawrence Lim <ll4715@nyu.edu>
2024-01-07 17:29:24 -0500

    initial commit

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,18 @@
+return {
+  -- Add colorscheme
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    opts = {
+      flavour = "macchiato", -- latte, frappe, macchiato, mocha
+    },
+  },
+
+  -- Configure LazyVim to load colorscheme
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin",
+    },
+  },
+}

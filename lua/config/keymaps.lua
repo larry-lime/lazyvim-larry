539b4b77d0f38520579c066fe7719633da2387f6
Lawrence Lim <ll4715@nyu.edu>
2024-01-08 17:35:33 -0500

    update

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -9,9 +9,18 @@ local jump_opt = { noremap = false, silent = true }
 
 -- Select all
 keymap("n", "<leader>A", "GVgg", opts)
+
+-- Buffer and tab navigation
+keymap("n", "<C-w>", ":q<CR>", opts)
+keymap("n", "<C-x>", ":bd!<CR>", opts)
+keymap("n", "<C-s>", "<C-^>", opts)
+
+-- Buffer and tab navigation
 keymap("n", "<leader>oa", ":%bd! | e# |bd#<CR>", opts)
-keymap("n", "<C-x>", ":q<CR>", opts)
+
+-- Copilot
 keymap("n", "<leader>ce", ":Copilot enable<CR>", opts)
 keymap("n", "<leader>cd", ":Copilot disable<CR>", opts)
 
--- keymap("n", "<C-x>", ":bd!<CR>", opts)
+-- Backspace
+keymap("i", "<M-BS>", "<C-W>", opts)
--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -5,12 +5,6 @@ return {
   },
   ---@param opts cmp.ConfigSchema
   opts = function(_, opts)
-    local has_words_before = function()
-      unpack = unpack or table.unpack
-      local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-      return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
-    end
-
     local luasnip = require("luasnip")
     local cmp = require("cmp")
 
@@ -40,6 +34,8 @@ return {
       ["<Tab>"] = cmp.mapping(function(fallback)
         if cmp.visible() then
           cmp.select_next_item()
+        elseif luasnip.jumpable(1) then
+          luasnip.jump(1)
         else
           fallback()
         end
